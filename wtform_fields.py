c357cb9cf9dc4616eeae234e2ab0b60ba6d4aa8d
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2019-12-16 21:54:58 +0200

    Creating form-helper and add Macros to registration page

--- wtform_fields.py
+++ wtform_fields.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField , PasswordField , SubmitField
 from wtforms.validators import InputRequired , Length , EqualTo
 
-
 class RegistrationForm(FlaskForm): 
 	username = StringField('username' , validators = [InputRequired(message='User Name Required') , Length(min=4 , max=25 , message="User Name must be between 4 and 25 characters")])
 	password = PasswordField('password' , validators = [InputRequired(message='Password Required') , Length(min=4 , max=25 , message="Password must be between 4 and 25 characters")])
